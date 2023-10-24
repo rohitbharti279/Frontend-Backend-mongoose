6c5c8ec989d0ab430f97ac257fcdd82f44901654
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-24 19:19:01 +0530

    conflict resolve

--- backend/server.js
+++ backend/server.js
@@ -8,14 +8,16 @@ const PORT = process.env.PORT || 3001;
 const cors = require('cors');
 app.use(cors());
 
-
+// handle incoming data from HTML
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://127.0.0.1:27017/web-quiz');
 
 
+ 
 
+// apply condition
 const dataSchema = new mongoose.Schema({
   name: String,
   age: Number,
